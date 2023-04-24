312375b24a0a1a642a5700ab41dea2b4f2f2ac53
anthonylieu <38957648+anthonylieu@users.noreply.github.com>
2023-04-24 02:56:49 -0400

    confirmed functional and commented

--- models/Comment.js
+++ models/Comment.js
@@ -1,8 +1,12 @@
+// Import required modules from Sequelize
 const { Model, DataTypes } = require('sequelize');
+// Import database connection configuration
 const sequelize = require('../config/connection');
 
+// Define the Comment class, which extends the Sequelize Model class
 class Comment extends Model {}
 
+// Initialize the Comment model with its schema and configuration
 Comment.init(
     {
         id: {
@@ -41,4 +45,5 @@ Comment.init(
     }
 )
 
+// Export the Comment model for use in other modules
 module.exports = Comment;
